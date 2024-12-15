c6e68b265e9cb8fb7d281cce8a8f4156d1138429
Neyza17 <nesyaratuanastasya@gmail.com>
2024-12-15 16:21:11 +0700

    first commit

---
+++ ArrayNilai17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArrayNilai17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai Akhir ke-" + i + ": ");
+            nilaiAkhir[i] =sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("NilaiAkhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        }
+    }
+}
