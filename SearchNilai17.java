12194a36c382f22bd211dd5d1a38ea4303c8c5bf
Neyza17 <nesyaratuanastasya@gmail.com>
2024-12-15 22:25:56 +0700

    second commit

--- SearchNilai17.java
+++ SearchNilai17.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = scanner.nextInt();
+        int[] arrNilai = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = scanner.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = scanner.nextInt();
+        int hasil = -1; 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i; 
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.err.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
+        scanner.close(); 
     }
 }
